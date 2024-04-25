64bdf83e0d5aff76081caedc45ccacb356997c83
gaoyan <magaoyanxj@gmail.com>
2024-04-25 23:36:21 +0800

    chore: 为QuestionList组件增加CSS样式

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,11 +10,35 @@ type PropsType = {
   createAt: string
 }
 const QuestionCard: FC<PropsType> = (props: PropsType) => {
-  const { _id } = props
+  const { _id, title, createAt, answerCount, isPublished } = props
   return (
-    <>
-      <p>QuestionCard {_id}</p>
-    </>
+    <div className={styles.container}>
+      {/* 上 */}
+      <div className={styles.title}>
+        <div className={styles.left}>
+          <a href="#">{title}</a>
+        </div>
+        <div className={styles.right}>
+          {isPublished ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
+          &nbsp;
+          <span>答卷：{answerCount}</span>
+          &nbsp;
+          <span>{createAt}</span>
+        </div>
+      </div>
+      {/* 下 */}
+      <div className={styles['button-container']}>
+        <div className={styles.left}>
+          <button>编辑问卷</button>
+          <button>数据统计</button>
+        </div>
+        <div className={styles.right}>
+          <button>标星</button>
+          <button>复制</button>
+          <button>删除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
