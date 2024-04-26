36f1f59a04ff3723e7de6bfd4df8e7390589629a
gaoyan <magaoyanxj@gmail.com>
2024-04-26 18:42:38 +0800

    chore: 使用antd开发首页组件

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,29 @@
 import React, { FC } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+import { Button, Typography } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+import styles from './Home.module.scss'
 
+const { Title, Paragraph } = Typography
 const Home: FC = () => {
   const navigate = useNavigate()
-  function handleLogin() {
-    // navigate('/login?b=20')
-    navigate({
-      pathname: '/login',
-      search: 'b=21',
-    })
-  }
+  // function handleLogin() {
+  //   // navigate('/login?b=20')
+  //   navigate({
+  //     pathname: '/login',
+  //     search: 'b=21',
+  //   })
+  // }
   return (
-    <div>
-      <p>Home</p>
-      <div>
-        <button onClick={handleLogin}>登录</button>
-        <Link to="register?a=10">注册</Link>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建问卷 100 份, 发布问卷 90份, 收到问卷980份</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => navigate(MANAGE_INDEX_PATHNAME)}>
+            开始使用
+          </Button>
+        </div>
       </div>
     </div>
   )
