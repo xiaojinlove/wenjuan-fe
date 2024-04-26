34411a55bc1b5d36e46c0c5be969a2a46f631d9a
gaoyan <magaoyanxj@gmail.com>
2024-04-26 22:21:55 +0800

    chore: 使用antd开发回收站页面-使用Table组件

--- src/pages/manage/Trash.tsx
+++ src/pages/manage/Trash.tsx
@@ -1,7 +1,93 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import styles from './common.module.scss'
+import QuestionCard from '../../components/QuestionCard'
+//import { useSearchParams } from 'react-router-dom'
+import { useTitle } from 'ahooks'
+import { Empty, Typography, Table, Tag } from 'antd'
 
+const { Title } = Typography
+const rawQuestionList = [
+  {
+    _id: 'q1',
+    title: '问卷1',
+    isPublished: false,
+    isStar: false,
+    answerCount: 5,
+    createAt: '3月10日 13:23',
+  },
+  {
+    _id: 'q2',
+    title: '问卷2',
+    isPublished: false,
+    isStar: true,
+    answerCount: 2,
+    createAt: '3月11日 13:23',
+  },
+  {
+    _id: 'q3',
+    title: '问卷3',
+    isPublished: false,
+    isStar: false,
+    answerCount: 7,
+    createAt: '4月10日 13:23',
+  },
+  {
+    _id: 'q4',
+    title: '问卷4',
+    isPublished: true,
+    isStar: true,
+    answerCount: 10,
+    createAt: '3月12日 13:23',
+  },
+]
+const tableColumns = [
+  {
+    title: '标题',
+    dataIndex: 'title',
+    //key: 'title', //循环列的 key ，他会默认取dataIndex的值
+  },
+  {
+    title: '是否发布',
+    dataIndex: 'isPublished',
+    render: (isPublished: boolean) => {
+      return isPublished ? <Tag color="processing">已发布</Tag> : <Tag>未发布</Tag>
+    },
+  },
+  {
+    title: '答卷',
+    dataIndex: 'answerCount',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createAt',
+  },
+]
 const Trash: FC = () => {
-  return <p>Trash</p>
+  useTitle('小星问卷 - 我的问卷')
+  // const [searchParams] = useSearchParams()
+  // console.log('keyword', searchParams)
+  const [questionList, setQuestionList] = useState(rawQuestionList)
+  return (
+    <>
+      {/* 上 */}
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>回收站</Title>
+        </div>
+        <div className={styles.right}>（搜索）</div>
+      </div>
+      {/* 中 */}
+      <div className={styles.content}>
+        {/* 问卷列表 */}
+        {questionList.length === 0 && <Empty description="暂无数据" />}
+        {questionList.length > 0 && (
+          <Table dataSource={questionList} columns={tableColumns} pagination={false} />
+        )}
+      </div>
+      {/* 下 */}
+      <div className={styles.footer}>分页</div>
+    </>
+  )
 }
 
 export default Trash
