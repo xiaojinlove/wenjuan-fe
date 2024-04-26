fc8cc75b350d1b526edf8833cfe0a80e5ecc1330
gaoyan <magaoyanxj@gmail.com>
2024-04-26 11:56:12 +0800

    chore: 使用antd重构MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,22 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Layout } from 'antd'
+import styles from './MainLayout.module.scss'
+
+const { Header, Content, Footer } = Layout
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>小星问卷 &copy;2024 - present. Created by xinfeng</Footer>
+    </Layout>
   )
 }
 
