9b0f36bd853492ceade896e4eca2ccbcdfc48e49
gaoyan <magaoyanxj@gmail.com>
2024-05-07 16:28:55 +0800

    chore: 使用AntD的Form组件开发登录页，并实现记住我功能

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,81 @@
-import React, { FC } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { FC, useEffect } from 'react'
+import styles from './Login.module.scss'
+import { Space, Typography, Form, Input, Button, Checkbox } from 'antd'
+import { UserAddOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { REGISTER_PATHNAME } from '../router'
+
+const { Title } = Typography
 
 const Login: FC = () => {
-  const navigate = useNavigate()
+  const [form] = Form.useForm() //第三方hook
+  useEffect(() => {
+    const { username, password } = getUserFromStorage()
+    form.setFieldsValue({ username, password })
+  }, [])
+  const USERNAME_KEY = 'USERNAME'
+  const PASSWORD_KEY = 'PASSWORD'
+  function rememberUser(username: string, password: string) {
+    localStorage.setItem(USERNAME_KEY, username)
+    localStorage.setItem(PASSWORD_KEY, password)
+  }
+  function deleteUserFromStorage() {
+    localStorage.removeItem(USERNAME_KEY)
+    localStorage.removeItem(PASSWORD_KEY)
+  }
+  function getUserFromStorage() {
+    return {
+      username: localStorage.getItem(USERNAME_KEY),
+      password: localStorage.getItem(PASSWORD_KEY),
+    }
+  }
+  function onFinish(values: any) {
+    //console.log(values)
+    const { username, password, remember } = values || {}
+    if (values.remember) {
+      //console.log('记住')
+      rememberUser(username, password)
+    } else {
+      //console.log('忘记')
+      deleteUserFromStorage()
+    }
+  }
   return (
-    <div>
-      <p>Login</p>
+    <div className={styles.container}>
       <div>
-        <button onClick={() => navigate(-1)}>返回</button>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onFinish}
+          initialValues={{ remember: true }}
+          form={form}
+        >
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }} name="remember" valuePropName="checked">
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )
