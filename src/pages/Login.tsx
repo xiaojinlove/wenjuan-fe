6835601639175bdfffd0f76d184927310001293c
gaoyan <magaoyanxj@gmail.com>
2024-05-07 16:53:55 +0800

    chore: 使用AntD的Form组件的校验功能,校验用户名和密码的格式

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -58,10 +58,22 @@ const Login: FC = () => {
           initialValues={{ remember: true }}
           form={form}
         >
-          <Form.Item label="用户名" name="username">
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              { type: 'string', min: 5, max: 20, message: '字符长度在 5-20 之间' },
+              { pattern: /^\w+$/, message: '只能是字母数字下划线' },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="密码" name="password">
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
             <Input.Password />
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 6, span: 16 }} name="remember" valuePropName="checked">
