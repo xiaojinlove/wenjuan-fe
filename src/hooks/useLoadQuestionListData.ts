7b400964682adc1244211db588f194dd3b30ccf8
gaoyan <magaoyanxj@gmail.com>
2024-05-09 20:20:30 +0800

    chore: 开发分页功能-定义URL参数page和pageSize

--- src/hooks/useLoadQuestionListData.ts
+++ src/hooks/useLoadQuestionListData.ts
@@ -1,7 +1,12 @@
 import { useRequest } from 'ahooks'
 import { getQuestionList } from '../services/question'
 import { useSearchParams } from 'react-router-dom'
-import { LIST_SEARCH_PARAM_KEY } from '../constant'
+import {
+  LIST_PAGE_PARAM_KEY,
+  LIST_PAGE_SIZE,
+  LIST_PAGE_SIZE_PARAM_KEY,
+  LIST_SEARCH_PARAM_KEY,
+} from '../constant'
 
 type OptionType = {
   isStar: boolean
@@ -13,7 +18,10 @@ function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
   const { loading, data, error } = useRequest(
     async () => {
       const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
-      const data = await getQuestionList({ keyword, isStar, isDeleted })
+      const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || '') || 1
+      const pageSize = parseInt(searchParams.get(LIST_PAGE_SIZE_PARAM_KEY) || '') || LIST_PAGE_SIZE
+
+      const data = await getQuestionList({ keyword, isStar, isDeleted, page, pageSize })
       return data
     },
     {
