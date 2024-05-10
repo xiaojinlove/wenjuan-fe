63ed217bcae39b63a4d84313f65008eec3267a36
gaoyan <magaoyanxj@gmail.com>
2024-05-10 10:09:23 +0800

    chore: loadMore上划加载更多-使用防抖监听scroll事件

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,11 +1,12 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import styles from './common.module.scss'
 import QuestionCard from '../../components/QuestionCard'
 //import { useSearchParams } from 'react-router-dom'
-import { useTitle } from 'ahooks'
+import { useDebounceFn, useTitle } from 'ahooks'
 import { Typography, Spin } from 'antd'
 import ListSearch from '../../components/ListSearch'
 import useLoadQuestionListData from '../../hooks/useLoadQuestionListData'
+import { useSearchParams } from 'react-router-dom'
 
 const { Title } = Typography
 
@@ -30,7 +31,32 @@ const List: FC = () => {
   const [list, setList] = useState([])
   const [total, setTotal] = useState(0)
   const haveMoreData = total > list.length
-
+  // function tryLoadMore() {
+  //   console.log('try load more...')
+  // }
+  const [searchParams] = useSearchParams()
+  //触发加载，防抖
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      console.log('try load more...')
+    },
+    {
+      wait: 1000,
+    }
+  )
+  //页面首次加载后和搜索关键词变化的时候会触发
+  useEffect(() => {
+    tryLoadMore()
+  }, [searchParams])
+  //当页面滚动时，触发加载
+  useEffect(() => {
+    if (haveMoreData) {
+      window.addEventListener('scroll', tryLoadMore)
+    }
+    return () => {
+      window.removeEventListener('scroll', tryLoadMore)
+    }
+  }, [searchParams])
   const { data = {}, loading } = useLoadQuestionListData()
   const { list = [], total = 0 } = data
   return (
