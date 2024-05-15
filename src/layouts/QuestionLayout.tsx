0b55ec413bf84a47901a087cdac3c41a9999db25
gaoyan <magaoyanxj@gmail.com>
2024-05-15 09:35:11 +0800

    chore: 根据用户登录状态动态跳转页面-使用自定义Hook统一处理

--- src/layouts/QuestionLayout.tsx
+++ src/layouts/QuestionLayout.tsx
@@ -2,9 +2,11 @@ import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import useLoadUserData from '../hooks/useLoadUserData'
 import { Spin } from 'antd'
+import useNavPage from '../hooks/useNavPage'
 
 const QuestionLayout: FC = () => {
   const { waitingUserData } = useLoadUserData()
+  useNavPage(waitingUserData)
   return (
     <div>
       <p>QuestionLayout left</p>
