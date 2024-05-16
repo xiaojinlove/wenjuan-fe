6e3728d03f2b56f01c67ee269084d8616e50ee21
gaoyan <magaoyanxj@gmail.com>
2024-05-16 10:25:56 +0800

    chore: 在画布上动态显示组件列表-梳理完整的流程

--- src/pages/question/Edit/EditCanvas.tsx
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -1,13 +1,24 @@
 import React, { FC } from 'react'
 import styles from './EditCanvas.module.scss'
-import QuestionTitle from '../../../components/QuestionComponents/QuestionTitle/Component'
-import QuestionInput from '../../../components/QuestionComponents/QuestionInput/Component'
 import { Spin } from 'antd'
 import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { ComponentInfoType } from '../../../store/componentsReducer'
+import { getComponentConfByType } from '../../../components/QuestionComponents'
 
 type PropsType = {
   loading: boolean
 }
+
+function genComponent(componentInfo: ComponentInfoType) {
+  const { type, props } = componentInfo // 每个组件的信息，是从 redux store 获取的（服务端获取）
+
+  const componentConf = getComponentConfByType(type)
+  if (componentConf == null) return null
+
+  const { Component } = componentConf
+  return <Component {...props} />
+}
+
 const EditCanvas: FC<PropsType> = ({ loading }) => {
   if (loading) {
     return (
@@ -16,12 +27,20 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
       </div>
     )
   }
+
   const { componentList } = useGetComponentInfo()
-  console.log(componentList)
 
   return (
     <div className={styles.canvas}>
-      <div className={styles['component-wrapper']}>
+      {componentList.map(c => {
+        const { fe_id } = c
+        return (
+          <div key={fe_id} className={styles['component-wrapper']}>
+            <div className={styles.component}>{genComponent(c)}</div>
+          </div>
+        )
+      })}
+      {/* <div className={styles['component-wrapper']}>
         <div className={styles.component}>
           <QuestionTitle />
         </div>
@@ -30,7 +49,7 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
         <div className={styles.component}>
           <QuestionInput />
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
