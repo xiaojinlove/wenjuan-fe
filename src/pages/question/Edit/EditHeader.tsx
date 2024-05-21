02fe4aaf21bc614926fd661ff19b6dcacb3c106e
gaoyan <magaoyanxj@gmail.com>
2024-05-21 09:18:23 +0800

    chore: 保存问卷信息-判断loading防止连续触发

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,12 +1,15 @@
 import React, { ChangeEvent, FC, useState } from 'react'
 import styles from './EditHeader.module.scss'
 import { Button, Input, Space, Typography } from 'antd'
-import { EditOutlined, LeftOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom'
+import { EditOutlined, LeftOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useNavigate, useParams } from 'react-router-dom'
 import EditToolbar from './EditToolbar'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
 import { useDispatch } from 'react-redux'
 import { changePageTitle } from '../../../store/pageInfoReducer'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+import { updateQuestionService } from '../../../services/question'
+import { useKeyPress, useRequest } from 'ahooks'
 
 const { Title } = Typography
 
@@ -41,6 +44,33 @@ const TitleElem: FC = () => {
   )
 }
 
+// 保存按钮
+const SaveButton: FC = () => {
+  const { id } = useParams()
+  const { componentList = [] } = useGetComponentInfo()
+  const pageInfo = useGetPageInfo()
+
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(id, { ...pageInfo, componentList })
+    },
+    { manual: true }
+  )
+
+  // 快捷键
+  useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+    event.preventDefault()
+    if (!loading) save()
+  })
+
+  return (
+    <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : null}>
+      保存
+    </Button>
+  )
+}
+
 //编辑器头部
 const EditHeader: FC = () => {
   const navigate = useNavigate()
@@ -62,7 +92,7 @@ const EditHeader: FC = () => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button>保存</Button>
+            <SaveButton />
             <Button type="primary">发布</Button>
           </Space>
         </div>
