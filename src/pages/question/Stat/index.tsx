03e9e7576e607d7b2ef6961fc7249dd410f6ab79
gaoyan <magaoyanxj@gmail.com>
2024-05-21 14:53:36 +0800

    chore: 页面UI布局-增加oading效果-part1

--- src/pages/question/Stat/index.tsx
+++ src/pages/question/Stat/index.tsx
@@ -4,6 +4,7 @@ import { Result, Spin, Button } from 'antd'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
 import { useNavigate } from 'react-router-dom'
 import { useTitle } from 'ahooks'
+import styles from './index.module.scss'
 
 const Stat: FC = () => {
   const { loading } = useLoadQuestionData()
@@ -12,32 +13,45 @@ const Stat: FC = () => {
 
   useTitle(`问卷统计 - ${title}`)
 
-  if (loading) {
+  // loading 效果
+  const LoadingELem = (
+    <div style={{ textAlign: 'center', marginTop: '60px' }}>
+      <Spin />
+    </div>
+  )
+  // Content Elem
+  function genContentElem() {
+    if (typeof isPublished === 'boolean' && !isPublished) {
+      return (
+        <div style={{ flex: '1' }}>
+          <Result
+            status="warning"
+            title="该页面尚未发布"
+            extra={
+              <Button type="primary" onClick={() => nav(-1)}>
+                返回
+              </Button>
+            }
+          ></Result>
+        </div>
+      )
+    }
+
     return (
-      <div style={{ textAlign: 'center', marginTop: '60px' }}>
-        <Spin />
-      </div>
+      <>
+        <div className={styles.left}></div>
+        <div className={styles.main}></div>
+        <div className={styles.right}></div>
+      </>
     )
   }
-  if (!isPublished) {
-    return (
-      <div style={{ flex: '1' }}>
-        <Result
-          status="warning"
-          title="该页面尚未发布"
-          extra={
-            <Button type="primary" onClick={() => nav(-1)}>
-              返回
-            </Button>
-          }
-        ></Result>
-      </div>
-    )
-  }
+
   return (
-    <div>
-      <p>Stat page</p>
-      {/* {loading ? <p>loading</p> : <p>{JSON.stringify(data)}</p>} */}
+    <div className={styles.container}>
+      <div className={styles['content-wrapper']}>
+        {loading && LoadingELem}
+        {!loading && <div className={styles.content}>{genContentElem()}</div>}
+      </div>
     </div>
   )
 }
