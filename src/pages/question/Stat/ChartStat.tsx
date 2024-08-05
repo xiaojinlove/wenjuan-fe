e44a3ca90c74bc1ff772b40ee654947056d52c5a
gaoyan <magaoyanxj@gmail.com>
2024-08-05 19:59:07 +0800

    chore: 设计图表统计的数据结构，并写到Mock服务中

--- src/pages/question/Stat/ChartStat.tsx
+++ src/pages/question/Stat/ChartStat.tsx
@@ -1,7 +1,10 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Typography } from 'antd'
 // import PieDemo from './PieDemo'
-import BarDemo from './BarDemo'
+import { useRequest } from 'ahooks'
+// import BarDemo from './BarDemo'
+import { getComponentStatService } from '../../../services/stat'
+import { useParams } from 'react-router-dom'
 
 const { Title } = Typography
 type PropsType = {
@@ -10,12 +13,30 @@ type PropsType = {
 }
 const ChartStat: FC<PropsType> = (props: PropsType) => {
   const { selectedComponentId, selectedComponentType } = props
+  const [stat, setStat] = useState([])
+  const { id = '' } = useParams()
+  const { run } = useRequest(
+    async (questionId, componentId) => await getComponentStatService(questionId, componentId),
+    {
+      manual: true,
+      onSuccess(res) {
+        setStat(res.stat)
+      },
+    }
+  )
+
+  useEffect(() => {
+    if (selectedComponentId) run(id, selectedComponentId)
+  }, [id, selectedComponentId])
+
+  function getStatElem() {
+    if (!selectedComponentId) return <div>未选中组件</div>
+    return <div>{JSON.stringify(stat)}</div>
+  }
   return (
     <>
       <Title level={3}>图表统计</Title>
-      <div>
-        <BarDemo />
-      </div>
+      <div>{getStatElem()}</div>
     </>
   )
 }
