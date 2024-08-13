284067bfe3c12f350c0421cac7dbee9b73e3ec6f
gaoyan <magaoyanxj@gmail.com>
2024-08-13 10:33:56 +0800

    chore: 抽离公共代码antd和react，合理使用HTTP缓存

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,34 @@
 module.exports = {
+  webpack: {
+    configure(webpackConfig) {
+      if (webpackConfig.mode === 'production') {
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            antd: {
+              name: 'antd-chunk',
+              test: /antd/,
+              priority: 100,
+            },
+            reactDom: {
+              name: 'reactDom-chunk',
+              test: /react-dom/,
+              priority: 99,
+            },
+            vendors: {
+              name: 'vendors-chunk',
+              test: /node_modules/,
+              priority: 98,
+            },
+          },
+        }
+      }
+      return webpackConfig
+    },
+  },
   devServer: {
     port: 8000, //B 端 前端
     proxy: {
